ba45b2b48b80ae776337107aa610673e23794913
suissa <jnascimento@gmail.com>
2012-10-01 20:29:03 -0300

    Listagem pelo Kaique

---
+++ app/js/controllers/product.js
@@ -0,0 +1,33 @@
+var ProductListControl = function ProductListControl ( $scope ) {
+    $scope.products = [{
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product two',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product three',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product four',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product five',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product six',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }];
+};
