7c9ac93404283d4212a4a87ef8cd5e84ec7edb2b
suissa <jnascimento@gmail.com>
2012-10-02 21:08:59 -0300

    subindo

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -3,10 +3,10 @@
 angular.module('order', ['mongoCategories', 'mongoProducts']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/categories', {templateUrl: '/angularjs-order-system/app/categories_list.html', controller: CategoryListCtrl}).
-      when('/categories/new', {templateUrl: '/angularjs-order-system/app/categories_new.html', controller: CategoryCreateCtrl}).
-      when('/categories/edit/:categoryId', {templateUrl: '/angularjs-order-system/app/categories_new.html', controller: CategoryEditCtrl}).
-      when('/products', {templateUrl: '/angularjs-order-system/app/product.html', controller: ProductListControl}).
+      when('/categories', {templateUrl: 'categories_list.html', controller: CategoryListCtrl}).
+      when('/categories/new', {templateUrl: 'categories_new.html', controller: CategoryCreateCtrl}).
+      when('/categories/edit/:categoryId', {templateUrl: 'categories_new.html', controller: CategoryEditCtrl}).
+      when('/products', {templateUrl: 'product.html', controller: ProductListControl}).
       //when('/products/new', {templateUrl: '/angularjs-order-system/app/product_new.html', controller: ProductCreateControl}).
       otherwise({redirectTo: '/'});
 }]);
